038d705d16eaf5c781b07e4f92129d17b84d829a
greduan <me@greduan.com>
2023-04-30 17:14:00 +0200

    fix(VehicleDto): make VinAuditData optional
    So that during the initial fetch of the vehicle, it doesn't need to be
    provided in the DTO.

--- VehicleGenius.Api/Dtos/VehicleDto.cs
+++ VehicleGenius.Api/Dtos/VehicleDto.cs
@@ -6,5 +6,5 @@ public class VehicleDto
 {
   public Guid Id { get; set; }
   public string Vin { get; set; }
-  public VinAuditData VinAuditData { get; set; }
+  public VinAuditData? VinAuditData { get; set; }
 }
