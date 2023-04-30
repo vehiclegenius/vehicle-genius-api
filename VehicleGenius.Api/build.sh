8f7b641588f02d9b468b3262cce0a762b195aef3
greduan <me@greduan.com>
2023-04-30 18:14:19 +0200

    chore: add environment and DB to build.sh

--- VehicleGenius.Api/build.sh
+++ VehicleGenius.Api/build.sh
@@ -4,8 +4,11 @@
 set -e
 
 export DOTNET_CLI_TELEMETRY_OPTOUT=1
+export ASPNETCORE_ENVIRONMENT=Production
 export PATH="$PATH:$HOME/.dotnet:$HOME/.dotnet/tools"
 
 rm -rf bin/
 dotnet restore
+dotnet tool restore
+dotnet ef database update
 dotnet build
