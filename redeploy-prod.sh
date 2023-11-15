9a2c1f278ea5365ee2caada26bfc07c17f782273
greduan <me@greduan.com>
2023-11-15 07:46:36 +0100

    chore: add prod scripts

---
+++ redeploy-prod.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Exit on first error
+set -e
+
+sudo supervisorctl stop vehicle-genius-api-prod || 0
+
+(cd VehicleGenius.Api && ./build.sh)
+
+sudo supervisorctl reread
+sudo supervisorctl update vehicle-genius-api-prod
+sudo supervisorctl restart vehicle-genius-api-prod
